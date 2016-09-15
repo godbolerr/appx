78ce93c3a9b881f65b648640cab120beed9969f0
godbolerr <godbolerr@gmail.com>
2016-09-15 23:16:01 +0530

    changed

--- src/main/java/com/appx/work/controller/AppxSeriesController.java
+++ src/main/java/com/appx/work/controller/AppxSeriesController.java
@@ -5,50 +5,25 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.appx.work.domain.Catalogue;
-import com.appx.work.service.AppxService;
+import com.appx.work.service.RuleService;
 
 @RestController
-@RequestMapping(value = "/rest/appx/catalogue")
+@RequestMapping(value = "/rest/appx/series")
 public class AppxSeriesController {
 
 	@Autowired
-	private AppxService service;
+	private RuleService service;
 
 	static Logger logger = LoggerFactory.getLogger(AppxSeriesController.class);
 
 	@RequestMapping(method = RequestMethod.POST)
-	public Catalogue addCatalogue(@RequestBody Catalogue catalogue) {
-		return service.addCatalogue(catalogue);
-	}
-
-	@RequestMapping(method = RequestMethod.PATCH)
-	public Catalogue editCatalogue(@RequestBody Catalogue catalogue) {
-
-		return service.updateCatalogue(catalogue);
-	}
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public boolean deleteCatalogue(@PathVariable("id") long id) {
-		return service.deleteCatalogue(id);
-	}
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Catalogue getCatalogue(@PathVariable("id") long id) {
-		return service.getCatalogueById(id);
-	}
-
-	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Catalogue> getCalaloguesList() {
-		return service.getCatalogues();
-
+	public List<Integer> getSeries(@RequestBody String rule) {
+		return service.getNumberSeries(rule);
 	}
 
 }
