c7d41ab778bdae00b8be9c5f56f1294b4125804b
godbolerr <godbolerr@gmail.com>
2016-09-15 23:15:44 +0530

    changes made

---
+++ src/main/java/com/appx/work/controller/AppxCatController.java
@@ -0,0 +1,54 @@
+package com.appx.work.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.appx.work.domain.Catalogue;
+import com.appx.work.service.AppxService;
+
+@RestController
+@RequestMapping(value = "/rest/appx/catalogue")
+public class AppxCatController {
+
+	@Autowired
+	private AppxService service;
+
+	static Logger logger = LoggerFactory.getLogger(AppxCatController.class);
+
+	@RequestMapping(method = RequestMethod.POST)
+	public Catalogue addCatalogue(@RequestBody Catalogue catalogue) {
+		return service.addCatalogue(catalogue);
+	}
+
+	@RequestMapping(method = RequestMethod.PATCH)
+	public Catalogue editCatalogue(@RequestBody Catalogue catalogue) {
+
+		return service.updateCatalogue(catalogue);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public boolean deleteCatalogue(@PathVariable("id") long id) {
+		return service.deleteCatalogue(id);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Catalogue getCatalogue(@PathVariable("id") long id) {
+		return service.getCatalogueById(id);
+	}
+
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Catalogue> getCalaloguesList() {
+		return service.getCatalogues();
+
+	}
+
+}
