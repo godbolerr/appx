37064124a9976361b03d4d5db1de665527978a5d
godbolerr@gmail.com <godbolerr@gmail.com>
2016-11-15 12:11:08 +0530

    changed controller to test

--- src/main/java/com/appx/work/controller/AppxController.java
+++ src/main/java/com/appx/work/controller/AppxController.java
@@ -1,60 +1,23 @@
 package com.appx.work.controller;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.appx.work.common.AppxUtil;
-import com.appx.work.service.AppxService;
+
+
 
 @RestController
 @RequestMapping(value = "/rest/appx/service")
 public class AppxController {
 
-	@Autowired
-	private AppxService service;
 	
-	private AppxService servic2;
-
 	static Logger logger = LoggerFactory.getLogger(AppxController.class);
 
 	@RequestMapping(value = "/seed", method = RequestMethod.GET)
 	public String seed() {
-
-		AppxUtil.service = service;
-
-		List<Long> ids = AppxUtil.loadDefinitionsFromFile(); //.loadDefinitions();
-
-		return ids.toString();
+		return "Got it";
 	}
-
-	@RequestMapping(value = "/load", method = RequestMethod.GET)
-	public String loadData() {
-
-		AppxUtil.service = service;
-		
-		System.out.println("Test load again for the first tim.");
-
-		List<Long> ids = AppxUtil.loadData();
-
-		return ids.toString();
-	}
-
-	@RequestMapping(value = "/seedAndLoad", method = RequestMethod.GET)
-	public String seedAndLoadData() {
-
-		AppxUtil.service = service;
-
-		AppxUtil.loadDefinitions();
-		
-		List<Long> ids = AppxUtil.loadData();
-
-		return ids.toString();
-	}
-
 }
